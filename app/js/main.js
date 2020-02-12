e39a3eef66358687807a3c261cc93da31c9c8c66
devolp <oleg600petr@gmail.com>
2020-02-12 18:23:55 +0200

    wednesday end work

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
-$(document).ready(function () {
-  $('.portfolio__slider-show-wrap').twentytwenty();
+$(document).ready(function() {
+    $('.portfolio__slider-show-wrap').twentytwenty();
+});
+
+$("#inputAreaSlider").slider({
+    range: "min",
+    value: 114,
+    min: 10,
+    max: 410,
+    slide: function(event, ui) {
+        $("#areaInput").val(ui.value);
+    }
+});
+$("#areaInput").val($("#inputAreaSlider").slider("value"));
+$(".calc__input-block select").selectmenu();
+$(".servise__item-header-btn").click(function() {
+    var selectedEffect = 'blind';
+    var options = {};
+    $(this).closest(".servise__item").children('.servise__item-content').toggle(selectedEffect, options, 500);
+    if ($(this).closest(".servise__item").hasClass("open-block")) {
+        $(this).closest(".servise__item").removeClass('open-block');
+    } else {
+        $(this).closest(".servise__item").addClass("open-block");
+    }
 });
