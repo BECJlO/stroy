5aab36e0f1a85f5916a7fb5daa0bf2f11acf77e4
devolp <oleg600petr@gmail.com>
2020-02-14 11:16:40 +0200

    Thursday home work

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
-    $('.portfolio__slider-show-wrap').twentytwenty();
+    $('.portfolio__slider-show-wrap').twentytwenty({
+        default_offset_pct: 0.35,
+        no_overlay: false,
+        before_label: '', // Set a custom before label
+        after_label: '',
+        move_slider_on_hover: false,
+        move_with_handle_only: false
+    });
+    $("#inputAreaSlider").slider({
+        range: "min",
+        value: 114,
+        min: 10,
+        max: 410,
+        slide: function(event, ui) {
+            $("#areaInput").val(ui.value);
+        }
+    });
+    $("#areaInput").val($("#inputAreaSlider").slider("value"));
+    $(".calc__input-block select").selectmenu();
+    $(".servise__item-header-btn").click(function() {
+        var selectedEffect = 'blind';
+        var options = {};
+        $(this).closest(".servise__item").children('.servise__item-content').toggle(selectedEffect, options, 500);
+        if ($(this).closest(".servise__item").hasClass("open-block")) {
+            $(this).closest(".servise__item").removeClass('open-block');
+        } else {
+            $(this).closest(".servise__item").addClass("open-block");
+        }
+    });
+    
 });
 
-$("#inputAreaSlider").slider({
-    range: "min",
-    value: 114,
-    min: 10,
-    max: 410,
-    slide: function(event, ui) {
-        $("#areaInput").val(ui.value);
-    }
-});
-$("#areaInput").val($("#inputAreaSlider").slider("value"));
-$(".calc__input-block select").selectmenu();
-$(".servise__item-header-btn").click(function() {
-    var selectedEffect = 'blind';
-    var options = {};
-    $(this).closest(".servise__item").children('.servise__item-content').toggle(selectedEffect, options, 500);
-    if ($(this).closest(".servise__item").hasClass("open-block")) {
-        $(this).closest(".servise__item").removeClass('open-block');
-    } else {
-        $(this).closest(".servise__item").addClass("open-block");
-    }
-});
